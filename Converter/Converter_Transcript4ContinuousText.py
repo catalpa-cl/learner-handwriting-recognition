05bd09477b0a6f90c5637583d04baa90561daf4e
UniDuEChristianGold <47823743+UniDuEChristianGold@users.noreply.github.com>
2023-06-22 11:37:07 +0200

    added Header Section

---
+++ Converter/Converter_Transcript4ContinuousText.py
@@ -0,0 +1,18 @@
+"""
+Converter_Transcrip4ContinuousText.py
+
+Author: Christian Gold
+Date: June, 2023
+
+Description: This file contains Python code that accomplishes a specific purpose.
+It serves as an implementation for the conversion from the handwritten transcriptions into a continuous text.
+
+For more details, please refer to the accompanying paper titled:
+
+Preserving the Authenticity of Handwritten Learner Language:
+Annotation Guidelines for Creating Transcripts Retaining Orthographic Features.
+by Christian Gold, Ronja Laarmann-Quante, Torsten Zesch
+at 1st Computation and Written Language (CAWL) Workshop at ACL. 2023
+
+"""
+
